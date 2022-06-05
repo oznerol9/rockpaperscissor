856c9f026c54a18dfb84d50815d4d165b99f4536
oznerol9 <lo.ludovici@gmail.com>
2022-06-05 14:57:04 +0200

    create function to select a randomplay from the computerPlay array

---
+++ app.js
@@ -0,0 +1,16 @@
+/* create array with computer moves and function to select a random option */
+
+var computerPlay = ['rock', 'paper', 'scissors'];
+
+function randomPlay() {
+    var randomNum = Math.floor(Math.random() * 3);
+    return(randomNum)
+}
+
+computerPlay[randomPlay()]
+
+/* function to play one round of rock paper and scissor */
+
+function play() {
+    
+}
