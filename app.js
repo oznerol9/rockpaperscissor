cfa85a12f89228074b94911c6e6aa2ea7fa01191
oznerol9 <lo.ludovici@gmail.com>
2022-06-07 08:42:13 +0200

    add function randomPlay(), play()

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 /* create array with computer moves and function to select a random option */
 
-var computerPlay = ['rock', 'paper', 'scissors'];
+function randomPlay(options) {
+    var options = ["rock", "paper", "scissors"]
+    return options[Math.floor(Math.random()*options.length)]
 
-function randomPlay() {
-    var randomNum = Math.floor(Math.random() * 3);
-    return(randomNum)
 }
 
-computerPlay[randomPlay()]
-
-/* function to play one round of rock paper and scissor */
+/* function to input playerChoice and play one round of rock paper and scissors */
 
 function play() {
+    var playerChoice = prompt("Make your choice: ")
+    var playerSelection = playerChoice.toLowerCase()
+
+    const computerChoice = randomPlay()
+
+    /* check if values are correct, to remove it */
+    console.log(playerChoice, computerChoice)
     
+    if (playerSelection == computerChoice) {
+        console.log("It's a tie!")
+    }
+    else {
+        console.log("It's not a tie!")
+    }
 }
