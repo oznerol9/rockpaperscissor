7b1f04fc3111a2e556bc3752794cbdf8336649d6
oznerol9 <lo.ludovici@gmail.com>
2022-06-08 12:05:38 +0200

    add play() function with for condition to end game when it reach 5 rounds

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
 /* create array with computer moves and function to select a random option */
 
-function randomPlay(options) {
-    var options = ["rock", "paper", "scissors"]
-    return options[Math.floor(Math.random()*options.length)]
+var playerScore = 0
+var computerScore = 0
 
+function randomPlay() {
+	var options = ["rock", "paper", "scissors"]
+	return options[Math.floor(Math.random()* options.length)]
 }
 
 /* function to input playerChoice and play one round of rock paper and scissors */
 
-function play() {
-    var playerChoice = prompt("Make your choice: ")
-    var playerSelection = playerChoice.toLowerCase()
+function playRound() {
+	var playerChoice = prompt("Make your choice: ")
+	var playerSelection = playerChoice.toLowerCase()
+	
+	const computerChoice = randomPlay()
+	
+	/* check if values are correct, to remove it */
+	console.log(playerChoice, computerChoice)
+	
+	if (playerSelection == computerChoice) {
+		console.log("It's a tie!")
+	}
+	else {
+		console.log("It's not a tie!")
+	}
+}
 
-    const computerChoice = randomPlay()
+function playRound() {
+	var playerChoice = prompt("Make your choice: ")
+	var playerSelection = playerChoice.toLowerCase()
+	
+	const computerChoice = randomPlay()
+	
+	/* check if values are correct, to remove it */
+	console.log(playerChoice, computerChoice)
+	
+	if (playerSelection == computerChoice) {
+		return result = "It's a tie! " + playerChoice + " and " + computerChoice + " are the same!"
+	}
+	else if (playerSelection == "rock" && computerChoice == "scissors" || 
+			playerSelection == "scissors" && computerChoice == "paper" ||
+			playerSelection == "paper" && computerChoice == "rock") {
+				playerScore++
+				return result = "Human kind has won! " + playerChoice + " beats " + computerChoice + " !"
+			}
+	else {
+		computerScore++
+		return result = "Robots have won! " + computerChoice + " beats " + playerChoice + " !"
+	}	
+}
 
-    /* check if values are correct, to remove it */
-    console.log(playerChoice, computerChoice)
-    
-    if (playerSelection == computerChoice) {
-        console.log("It's a tie!")
-    }
-    else {
-        console.log("It's not a tie!")
-    }
+function game() {
+	for (let i = 0; i < 5; i++) {
+		playRound()
+	}
+	
+	if (playerScore < computerScore) {
+		console.log("Robots have won! Mankind is doomed")
+	}
+	else if (playerScore == computerScore) {
+		console.log("Nobody won!")
+	}
+	else {
+		console.log("Mankin has won! Robots are... still useful")
+	}
 }
